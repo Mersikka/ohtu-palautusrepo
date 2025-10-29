c142a70e6ff498b5b3d44f49f1328446981e84df
mersikka <mio@argillander.me>
2025-10-29 18:55:35 +0200

    Test class StatisticsService

---
+++ viikko1/nhl-statistics/src/tests/statisticsservice_test.py
@@ -0,0 +1,33 @@
+import unittest
+from statistics_service import StatisticsService
+from player import Player
+
+class PlayerReaderStub:
+    def get_players(self):
+        return [
+            Player("Semenko", "EDM", 4, 12),  #  4+12 = 16
+            Player("Lemieux", "PIT", 45, 54), # 45+54 = 99
+            Player("Kurri",   "EDM", 37, 53), # 37+53 = 90
+            Player("Yzerman", "DET", 42, 56), # 42+56 = 98
+            Player("Gretzky", "EDM", 35, 89)  # 35+89 = 124
+        ]
+
+class TestStatisticsService(unittest.TestCase):
+    def setUp(self):
+        # annetaan StatisticsService-luokan oliolle "stub"-luokan olio
+        self.stats = StatisticsService(
+            PlayerReaderStub()
+        )
+
+    def test_player_found(self):
+        self.assertIsNotNone(self.stats.search("Kurri"))
+
+    def test_fake_player_not_found(self):
+        self.assertIsNone(self.stats.search("Mersikka"))
+
+    def test_search_by_team(self):
+        self.assertEqual(len(self.stats.team("EDM")), 3)
+
+    def test_top(self):
+        self.assertEqual(self.stats.top(1)[0].name, "Gretzky")
+
