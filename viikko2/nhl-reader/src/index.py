99110863a7b84bc3587686d25040cb5a8c0972b7
mersikka <mio@argillander.me>
2025-11-05 20:44:18 +0200

    Bleurgh

--- viikko2/nhl-reader/src/index.py
+++ viikko2/nhl-reader/src/index.py
@@ -1,7 +1,17 @@
+import requests
 from player import Player
 
 def main():
-    pass
+    url = "https://studies.cs.helsinki.fi/nhlstats/2024-25/players"
+    response = requests.get(url).json()
+
+    players = sorted(list(filter(lambda p: p.nationality == "FIN", list(map(lambda p: Player(p), response)))), key=lambda p: p.goals + p.assists, reverse=True)
+
+    print("Players from FIN:\n")
+
+    for player in players:
+        print(player)
+
 
 if __name__ == "__main__":
     main()
