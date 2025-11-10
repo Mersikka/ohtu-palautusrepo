f482f304b5232d0e6b0b388c5c16b1a459cf12e9
mersikka <mio@argillander.me>
2025-11-10 15:49:58 +0200

    Do ex. 4

--- viikko2/nhl-reader/src/index.py
+++ viikko2/nhl-reader/src/index.py
@@ -1,17 +1,37 @@
+from tarfile import SUPPORTED_TYPES
 import requests
 from player import Player
 
+class PlayerReader:
+    def __init__(self, url):
+        self.url = url
+        self.response = requests.get(url).json()
+
+class PlayerStats:
+    def __init__(self, reader):
+        self.response = reader.response
+
+    def top_scorers_by_nationality(self, nat):
+        filtered_ps = list(filter(lambda p: p["nationality"]==nat, self.response))
+        sorted_ps = sorted(filtered_ps, key=lambda p: p["goals"]+p["assists"], reverse=True)
+        sorted_ps = list(map(lambda p: Player(p), sorted_ps))
+        return sorted_ps
+    
 def main():
+    # url = "https://studies.cs.helsinki.fi/nhlstats/2024-25/players"
+    # response = requests.get(url).json()
+
+    # players = sorted(list(filter(lambda p: p.nationality == "FIN", list(map(lambda p: Player(p), response)))), key=lambda p: p.goals + p.assists, reverse=True)
+
+    # print("Players from FIN:\n")
+
     url = "https://studies.cs.helsinki.fi/nhlstats/2024-25/players"
-    response = requests.get(url).json()
-
-    players = sorted(list(filter(lambda p: p.nationality == "FIN", list(map(lambda p: Player(p), response)))), key=lambda p: p.goals + p.assists, reverse=True)
-
-    print("Players from FIN:\n")
+    reader = PlayerReader(url)
+    stats = PlayerStats(reader)
+    players = stats.top_scorers_by_nationality("FIN")
 
     for player in players:
         print(player)
-
 
 if __name__ == "__main__":
     main()
