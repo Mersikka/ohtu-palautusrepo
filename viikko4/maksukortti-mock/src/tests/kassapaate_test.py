b0ad9c05bfa6a9dd9e317bc18ac2f6f668c0fc11
Mersikka <mio@argillander.me>
2025-11-21 22:31:33 +0200

    Complete ex. 2

--- viikko4/maksukortti-mock/src/tests/kassapaate_test.py
+++ viikko4/maksukortti-mock/src/tests/kassapaate_test.py
@@ -23,3 +23,17 @@ class TestKassapaate(unittest.TestCase):
         self.kassa.osta_lounas(maksukortti_mock)
 
         maksukortti_mock.osta.assert_not_called()
+
+    def test_saldo_nousee_jos_lataus_positiivinen(self):
+        maksukortti_mock = Mock()
+
+        self.kassa.lataa(maksukortti_mock, 7)
+
+        maksukortti_mock.lataa.assert_called_with(7)
+
+    def test_saldo_ei_muutu_jos_lataus_negatiivinen(self):
+        maksukortti_mock = Mock()
+
+        self.kassa.lataa(maksukortti_mock, -7)
+
+        maksukortti_mock.lataa.assert_not_called()
