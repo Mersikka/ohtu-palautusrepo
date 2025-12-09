1e6f44a22596e0f0b9688eb4a060145c17ce6819
mersikka <mio@argillander.me>
2025-12-09 13:55:24 +0200

    Complete ex4

--- viikko6/query-language/src/matchers.py
+++ viikko6/query-language/src/matchers.py
@@ -9,6 +9,16 @@ class And:
 
         return True
 
+class Or:
+    def __init__(self, *matchers):
+        self._matchers = matchers
+
+    def test(self, player):
+        for matcher in self._matchers:
+            if matcher.test(player):
+                return True
+
+        return False
 
 class PlaysIn:
     def __init__(self, team):
@@ -27,3 +37,22 @@ class HasAtLeast:
         player_value = getattr(player, self._attr)
 
         return player_value >= self._value
+
+class All:
+    def test(self, player):
+        return True
+
+class Not:
+    def __init__(self, matcher):
+        self._matcher = matcher
+
+    def test(self, player):
+        return not self._matcher.test(player)
+
+class HasFewerThan(Not):
+    def __init__(self, value, attr):
+        self._value = value
+        self._attr = attr
+
+    def test(self, player):
+        return Not(HasAtLeast(self._value, self._attr))
